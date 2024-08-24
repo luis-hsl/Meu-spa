6eb8d299c316af09feb269d62ac7cbb1d1b73398
luis-hsl <luis_henrique04c@hotmail.com>
2024-08-24 01:44:25 -0300

    Adiciona componente Cabecalho com título e componente Menu

---
+++ src/Components/Cabecalho/Cabecalho.tsx
@@ -0,0 +1,10 @@
+import Menu from "../Menu/Menu";
+
+export default function Cabecalho() {
+    return (
+        <header>
+            <h1>Cabeçalho</h1>
+            <Menu />
+        </header>
+    );
+}
