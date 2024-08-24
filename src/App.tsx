f43c1b015651cb8d4514f3bcb5a1264c105d88ef
luis-hsl <luis_henrique04c@hotmail.com>
2024-08-24 01:47:22 -0300

    Adiciona componente App com Cabecalho, Rodape e Outlet para renderização de rotas

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { Outlet } from "react-router-dom";
-import Cabecalho from "./Components/Cabecalho";
-import Rodape from "./Components/Rodape";
+import Cabecalho from "./Components/Cabecalho/Cabecalho";
+import Rodape from "./Components/Rodape/Rodape";
 
 export default function App() {
   return (
